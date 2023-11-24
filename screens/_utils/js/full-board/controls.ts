1754406999f37ff1da10225fef7ea0bb0799e871
Murillo Nicacio de Maraes <mnmaraes@gmail.com>
2023-11-24 13:49:31 +0000

    chore: improved full board game tick

--- screens/_utils/js/full-board/controls.ts
+++ screens/_utils/js/full-board/controls.ts
@@ -1,51 +1,74 @@
-function cellState(boardState: boolean[][], x: number, y: number) {
-  const isAlive = boardState[y][x]
-
-  let neighbors = 0
-  for (let i = -1; i <= 1; i++) {
-    for (let j = -1; j <= 1; j++) {
-      const neighborX = x + i
-      const neighborY = y + j
-
-      if (neighborX < 0 || neighborX >= boardState[0].length || neighborY < 0 || neighborY >= boardState.length) {
-        continue
-      }
-
-      if (i === 0 && j === 0) {
-        continue
-      }
-
-      if (boardState[neighborY][neighborX]) {
-        neighbors++
-      }
-    }
-  }
-
-  return {
-    isAlive,
-    neighbors,
-  }
-}
-
 export function gameTick(boardState: boolean[][]) {
-  const newState = []
+  const newBoardState = [...boardState.map(row => Array(row.length).fill(false))]
+  const sparseCounter: {[col: number]: {[row: number]: {isAlive: boolean; neighbors: number}}} = {}
 
   for (let i = 0; i < boardState.length; i++) {
-    const newRow = []
-    for (let j = 0; j < boardState[0].length; j++) {
-      const {isAlive, neighbors} = cellState(boardState, j, i)
+    for (let j = 0; j < boardState[i].length; j++) {
+      if (!boardState[i][j]) {
+        continue
+      }
 
-      if (!isAlive && neighbors === 3) {
-        newRow.push(true)
-      } else if (isAlive && (neighbors < 2 || neighbors > 3)) {
-        newRow.push(false)
+      // Increment neighbors' counts
+      for (let k = -1; k <= 1; k++) {
+        for (let l = -1; l <= 1; l++) {
+          const x = j + k
+          const y = i + l
+
+          if (x < 0 || x >= boardState[i].length || y < 0 || y >= boardState.length) {
+            continue
+          }
+
+          if (k === 0 && l === 0) {
+            continue
+          }
+
+          if (!sparseCounter[y]) {
+            sparseCounter[y] = {}
+          }
+
+          if (!sparseCounter[y][x]) {
+            sparseCounter[y][x] = {
+              isAlive: false,
+              neighbors: 0,
+            }
+          }
+
+          sparseCounter[y][x].neighbors++
+        }
+      }
+
+      // Set self to alive
+      if (!sparseCounter[i]) {
+        sparseCounter[i] = {}
+      }
+
+      if (!sparseCounter[i][j]) {
+        sparseCounter[i][j] = {
+          isAlive: true,
+          neighbors: 0,
+        }
       } else {
-        newRow.push(isAlive)
+        sparseCounter[i][j].isAlive = true
       }
     }
-
-    newState.push(newRow)
   }
 
-  return newState
+  // Get new board state
+  Object.keys(sparseCounter).forEach(iStr => {
+    const i = Number(iStr)
+
+    const row = sparseCounter[i]
+
+    Object.keys(row).forEach(jStr => {
+      const j = Number(jStr)
+
+      const {isAlive, neighbors} = row[j]
+
+      if ((!isAlive && neighbors === 3) || (isAlive && (neighbors === 2 || neighbors === 3))) {
+        newBoardState[i][j] = true
+      }
+    })
+  })
+
+  return newBoardState
 }
